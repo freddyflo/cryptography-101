2fb63a14048f211c347391b902dc18876f7e4dda
Fred Aklamanu <fredaklamanu@google.com>
2025-07-03 07:05:07 +0200

    encryption + decryption fx

--- onetimepad.py
+++ onetimepad.py
@@ -1,12 +1,40 @@
 # use secret module instead, random module is for demonstration purposes
 from random import randint
+from frequencyanalysiscaesar import plot_distribution, frequency_analysis
 
-ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def encrypt(plain_text):
+def encrypt(plain_text, key):
+
+    plain_text = plain_text.upper()
+    
+    cipher_text = ''
+
+    for index, character in enumerate(plain_text):
+        key_index = key[index]
+        character_index = ALPHABET.find(character)
+
+        # Ei(xi) = (xi + OTPi) mod 26
+        cipher_text += ALPHABET[(character_index + key_index) % len(ALPHABET)]
+
+    return cipher_text
+
     pass
 
-def decrypt(cipher_text):
+def decrypt(cipher_text, key):
+
+    plain_text = ''
+
+    for index, character in enumerate(cipher_text):
+        
+        key_index = key[index]  
+        character_index = ALPHABET.find(character)
+        
+        # Di(xi) = (xi - OTPi) mod 26
+        plain_text += ALPHABET[(character_index - key_index) % len(ALPHABET)]
+
+    return plain_text
+
     pass
 
 def random_sequence(text):
@@ -20,4 +48,16 @@ def random_sequence(text):
 
 
 if __name__ == '__main__':
-    print(random_sequence("Hello World"))
+    
+    message = "Shannon defined the quantity of information produced by a source--for example, the quantity in a message--by a formula similar to the equation that defines thermodynamic entropy in physics. In its most basic terms, Shannon's informational entropy is the number of binary digits required to encode a message"
+    sequence = random_sequence(message)
+    print("#" * 50)
+    print("Original message: %s" % message.upper())
+    cipher = encrypt(message, sequence)
+    print("#" * 50)
+    print("Encrypted message: %s" % cipher) 
+    print("#" * 50)
+    print("Decrypted message: %s" % decrypt(cipher, sequence))
+    print("#" * 50)
+    plot_distribution(frequency_analysis(cipher))
+    print("#" * 50)
